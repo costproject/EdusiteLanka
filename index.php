6991c0a8c10f145fcbc8ac82e5f08bdd96b4ef64
Sandun-S <58127121+Sandun-S@users.noreply.github.com>
2023-03-04 20:31:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!-- -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HomePage</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
